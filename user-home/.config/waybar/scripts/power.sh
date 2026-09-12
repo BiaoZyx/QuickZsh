c43e55ab8f420cc7b6a12aa5660940e7b846dc69
BiaoZyx <BiaoZyx@outlook.com>
2026-09-12 11:51:54 +0800

    微调，全局fuzzel

--- user-home/.config/waybar/scripts/power.sh
+++ user-home/.config/waybar/scripts/power.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-choice=$(printf "Lock\nLogout\nSleep\nReboot\nShutdown" | rofi -dmenu -p "Power")
+#choice=$(printf "Lock\nLogout\nSleep\nReboot\nShutdown" | rofi -dmenu -p "Power")
+choice=$(printf "Lock\nLogout\nSleep\nReboot\nShutdown" | fuzzel --dmenu -p "Power | ")
 
 if [ -z "$choice" ]; then
-    exit 0
+  exit 0
 fi
 
 case "$choice" in
-    Lock) swaylock ;;
-    Logout) swaymsg exit ;;
-    Sleep) swaylock & loginctl suspend ;;
-    Reboot) dbus-run-session loginctl reboot ;;
-    Shutdown) dbus-run-session loginctl poweroff ;;
+Lock) swaylock ;;
+Logout) swaymsg exit ;;
+Sleep)
+  swaylock &
+  loginctl suspend
+  ;;
+Reboot) dbus-run-session loginctl reboot ;;
+Shutdown) dbus-run-session loginctl poweroff ;;
 esac
